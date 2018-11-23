652abb25f135c9b1c82904c22c1488dd9c5a88b8
wskiawv <wskiawv@163.com>
2018-11-23 17:23:32 +0800

    添加表结构生成sql语句

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,181 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : edms
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2018-11-23 17:21:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `t_attachment`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_attachment`;
+CREATE TABLE `t_attachment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `storage_type` varchar(255) DEFAULT NULL,
+  `att_name` varchar(255) DEFAULT NULL,
+  `att_size` int(11) DEFAULT NULL,
+  `att_path` varchar(255) DEFAULT NULL,
+  `att_type` varchar(200) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `last_update_date` datetime DEFAULT NULL,
+  `last_update_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_attachment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_document`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_document`;
+CREATE TABLE `t_document` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `document_name` varchar(255) DEFAULT NULL,
+  `document_title` varchar(255) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `last_update_date` datetime DEFAULT NULL,
+  `last_update_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_document
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_resources`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_resources`;
+CREATE TABLE `t_resources` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `ctl` varchar(255) DEFAULT NULL,
+  `iconCls` varchar(255) DEFAULT NULL,
+  `leaf` bit(1) DEFAULT NULL,
+  `method` varchar(255) DEFAULT NULL,
+  `methodName` varchar(255) DEFAULT NULL,
+  `modules` varchar(255) DEFAULT NULL,
+  `modulesName` varchar(255) DEFAULT NULL,
+  `orderNum` int(11) DEFAULT NULL,
+  `qtip` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `xtype` varchar(255) DEFAULT NULL,
+  `pid` int(20) DEFAULT NULL,
+  `display` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_resources
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(200) DEFAULT NULL,
+  `role_type` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `last_update_date` datetime DEFAULT NULL,
+  `last_update_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_role_resources`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role_resources`;
+CREATE TABLE `t_role_resources` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `resourcesId` int(20) DEFAULT NULL,
+  `roleId` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_role_resources
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_storage`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_storage`;
+CREATE TABLE `t_storage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `storage_name` varchar(200) DEFAULT NULL,
+  `config_file` varchar(200) DEFAULT NULL,
+  `storage_type` varchar(200) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_storage
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `rmb` decimal(10,0) DEFAULT NULL,
+  `usd` decimal(10,0) DEFAULT NULL,
+  `activateCode` varchar(100) DEFAULT NULL,
+  `uuid` varchar(200) DEFAULT NULL,
+  `homeUrl` varchar(200) DEFAULT NULL,
+  `imageUrl` varchar(200) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `create_by` int(11) DEFAULT NULL,
+  `last_update_date` datetime DEFAULT NULL,
+  `last_update_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_user_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_role`;
+CREATE TABLE `t_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user_role
+-- ----------------------------
